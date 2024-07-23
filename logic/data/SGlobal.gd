8656521ffdf0d4f35e0b724e5185687800198a08
duarterte <katmandu612@gmail.com>
2024-07-23 07:48:04 -0400

    Game menu

---
+++ logic/data/SGlobal.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var gameName = "Game Title in res://logic/data/SGlobal.gd"
+func _ready():
+	pass
---
+++ logic/ui/MainMenu.gd
@@ -0,0 +1,11 @@
+extends Control
+@onready var gametTilteLabel: Label = $VBoxContainer/CenterContainer/GametTitle
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if SGlobal.gameName: gametTilteLabel.text = SGlobal.gameName
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
